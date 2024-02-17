46b9ef00bb969aff2a193c4c32c6fbeb78edd23c
Aman Singh <as3189997@gmail.com>
2024-02-17 11:20:59 +0530

    Error handled

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,14 +1,35 @@
 package com.driver;
 
 public class Boat implements  WaterVehicle{
+    private int VehicleCapacity;
+    private String VehicleName;
+
+    public Boat(int vehicleCapacity, String vehicleName) {
+        VehicleCapacity = vehicleCapacity;
+        VehicleName = vehicleName;
+    }
 
     @Override
     public int getVehicleCapacity() {
-        return 4;
+        return VehicleCapacity;
+    }
+
+    public void setVehicleCapacity(int vehicleCapacity) {
+        VehicleCapacity = vehicleCapacity;
     }
 
     @Override
     public String getVehicleName() {
-        return "SUV";
+        return VehicleName;
     }
+
+    public void setVehicleName(String vehicleName) {
+        VehicleName = vehicleName;
+    }
+
+    public Boat() {
+
+    }
+
+
 }
